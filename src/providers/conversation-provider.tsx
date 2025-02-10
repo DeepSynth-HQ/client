09a44e3c5a5d7c934b1cb71d3e9c6eef39a3fc84
bakansm <khuynhbao5@gmail.com>
2025-02-10 18:53:28 +0700

    update: metadata

--- src/providers/conversation-provider.tsx
+++ src/providers/conversation-provider.tsx
@@ -43,6 +43,9 @@ export const ConversationProvider = ({
         session_id: sessionId,
       };
 
+      console.log("payload: ", payload);
+      console.log("bearer token: ", session?.token);
+
       try {
         setIsFetchingConversation(true);
         const serverUrl = `${SERVER_URL}/agent/history`;
@@ -63,6 +66,7 @@ export const ConversationProvider = ({
         }
 
         const conversation: Conversation[] = await response.json();
+        console.log(conversation);
         setConversation(conversation);
       } catch (error) {
         console.error("Error:", error);
@@ -77,6 +81,7 @@ export const ConversationProvider = ({
       setIsFetchingConversation,
     ],
   );
+
   const fetchConversationSessions = useCallback(async () => {
     if (!session) return;
     try {
